243248f7622278b6a995a646da74d852cc6acc46
orzino3 <155395782+orzino3@users.noreply.github.com>
2024-06-25 15:43:13 +0300

    Update MainWindow.java
    update main window settings

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,16 +7,12 @@ import java.io.File;
 
 public class MainWindow extends JFrame {
 
-    public static final int ERROR_MARGIN = 40;
-    public final int BUTTON_WIDTH = 100;
-    public final int BUTTON_HEIGHT = 50;
-
     private File fileToOpen;
     private boolean isGoodToGo;
 
     public MainWindow() {
         MainPanel panel = new MainPanel();
-        this.setSize(panel.getBackgroundImage().getWidth(panel), panel.getBackgroundImage().getHeight(panel) + ERROR_MARGIN);
+        this.setSize(panel.getBackgroundImage().getWidth(panel), panel.getBackgroundImage().getHeight(panel) + MainPanel.MARGIN);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setLayout(null);
@@ -27,11 +24,8 @@ public class MainWindow extends JFrame {
 
 
         JButton start = new JButton("פתח תמונה");
-       // start.setBounds((int) (this.getWidth() / 5) + MainWindow.ERROR_MARGIN, (int) (this.getHeight() / 2), BUTTON_WIDTH, BUTTON_HEIGHT);
-        //this.add(start);
         this.add(start, BorderLayout.SOUTH);
         mainAction(start);
-        //this.add(panel);
         this.add(panel, BorderLayout.CENTER);
 
     }
@@ -41,28 +35,33 @@ public class MainWindow extends JFrame {
         start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // צור JFileChooser
+                //Creating a JFileChooser that opens the file explorer in InitialPath Location
+                //Filter type of files in JFileChoose using FileNameExtensionFilter class
+                File initialPath = new File("C:\\");
                 JFileChooser fileChooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Jpeg & PNG Images", "jpg", "png", "gif");
+                fileChooser.setFileFilter(filter);
+                fileChooser.setCurrentDirectory(initialPath);
                 fileChooser.setDialogTitle("Select a file");
 
-                // פתח את תיבת הבחירה והמתן לבחירת הקובץ
                 int userSelection = fileChooser.showOpenDialog(null);
 
-                // בדוק אם המשתמש בחר קובץ
+                //Checking if there is a user selection, and showing file name and path
+                //If a file has been selected, boolean isGoodToGo changes to TRUE
                 if (userSelection == JFileChooser.APPROVE_OPTION) {
                     fileToOpen = fileChooser.getSelectedFile();
-                    // הצג את שם הקובץ שנבחר
-                    JOptionPane.showMessageDialog(null, "Selected file: " + fileToOpen.getAbsolutePath());
+
+                    JOptionPane.showMessageDialog(null, "Selected file: " +
+                            fileToOpen.getName() + "\nFile Path is: " + fileToOpen.getAbsolutePath());
                     isGoodToGo = true;
                 }
 
+                //If isGoodToGo is TRUE we can proceed to next stage of image manipulation by opening ImageFrame, and disposing the MainWindow
                 if (isGoodToGo) {
                     new ImageFrame(fileToOpen);
                     dispose();
                 }
             }
         });
-
-
     }
 }
